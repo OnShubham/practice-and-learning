7bdaf6ebd7b6a7ef8aa3961f2304b4b757272c20
OnShubham <shubhamkharche3005@gmail.com>
2024-03-12 00:56:42 +0530

    completed

--- Complete Python Developer  - ZTM/Function.py
+++ Complete Python Developer  - ZTM/Function.py
@@ -1,6 +1,10 @@
 # Fucntional Programming
 # Pure Functions
 
+import functools
+from functools import reduce
+
+
 def multiply_by2(li):
     new_list = []
     for item in li:
@@ -52,3 +56,54 @@ for product in listed_products:
     print(product)
 
 
+# Mao, Filter, Zip, Reduce
+
+    def multiply_by2(x):
+        return x * 2
+
+    print(list(map(multiply_by2, [1, 2, 3,])))
+
+# filter
+
+    def only_odd(item):
+        return item % 2 != 0
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list(filter(only_odd, my_list)))
+
+# Zip
+
+zip_list = [1, 2, 3]
+zip_list2 = [10, 20, 30]
+
+print(list(zip(zip_list, zip_list2)))
+
+
+# string
+
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy", "Monica", "Vicky")
+
+print(str(zip(a, b)))
+
+
+# reduce
+# importing functools for reduce()
+
+# initializing list
+lis = [1, 3, 5, 6, 2]
+
+# using reduce to compute sum of list
+print("The sum of the list elements is : ", end="")
+print(functools.reduce(lambda a, b: a+b, lis))
+
+# using reduce to compute maximum element from list
+print("The maximum element of the list is : ", end="")
+print(functools.reduce(lambda a, b: a if a > b else b, lis))
+
+# lambda function
+
+my_list = [1, 2, 3]
+
+new_list = list(map(lambda item: item*2, my_list))
+print(new_list)
